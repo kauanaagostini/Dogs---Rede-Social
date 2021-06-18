7c0d4ab3c41ec7f78d2974a8b27d082c3f5b4280
Kauana Maria Agostini <kauanaagostini@gmail.com>
2021-06-18 14:53:38 -0300

    criado contexto para lidar com login globalmente podendo buscar os dados do usuario logado em qualquer parte da aplicação.

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,35 +3,19 @@ import { Link } from 'react-router-dom'
 import Input from '../FormsComponents/Input/Input'
 import Button from '../FormsComponents/Button/Button'
 import useForm from '../../Hooks/useForm'
-import { TOKEN_POST, USER_GET } from '../../api'
+import { UserContext } from '../../Context/UserContext'
 
 const LoginForm = () => {
   const username = useForm()
   const password = useForm()
 
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token')
-    if(token) {
-      getUser(token)
-    }
-  }, [])
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token)
-    const response = await fetch(url, options);
-    const json = await response.json()
-    console.log(json)
-  }
+  const {userLogin} = React.useContext(UserContext)
 
   async function handleLogin(e) {
     e.preventDefault()
 
     if(username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({ username: username.value, password: password.value})
-
-      const respose = await fetch(url, options)
-      const json = await respose.json()
-      window.localStorage.setItem('token', json.token)
+      userLogin(username.value, password.value)
     }
   }
 
