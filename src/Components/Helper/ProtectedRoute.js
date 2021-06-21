ecdad340c21dab54ab6237d404ab8ee47eb7969f
Kauana Maria Agostini <kauanaagostini@gmail.com>
2021-06-21 19:43:06 -0300

    criado menus de navegação do componente usuario, já com seus estilos.

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -4,7 +4,6 @@ import { Navigate, Route } from 'react-router-dom'
 
 const ProtectedRoute = (props) => {
   const { login } = React.useContext(UserContext)
-  console.log(login)
 
   if(login === true) return <Route {...props} />;
   else if(login === false) return <Navigate to="/login" />;
